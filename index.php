8d05992a5e74ad3cacca53400894c0332ab97dce
Daniel Scheuermann <Daniel.Scheuermann@uvm.edu>
2022-11-28 20:22:34 -0500

    Added row sorting customizability

--- index.php
+++ index.php
@@ -5,9 +5,14 @@ include 'top.php';
 <section class="arraySection">
 
     <?php
+
+    $order = isset($_GET['order'])?$_GET['order']:'pmkEmployeeId';
+    $direction = isset($_GET['direction'])?$_GET['direction']:'ASC';
+    $page = isset($_GET['page'])?$_GET['page']:0;
+    
     $sql = 'SELECT pmkEmployeeId, fldName, fldSalary, fldHighestDegree ';
     $sql .= 'FROM tblEmployees ';
-    $sql .= 'ORDER BY pmkEmployeeId';
+    $sql .= 'ORDER BY ' . $order . ' ' . $direction;
 
     $records = $thisDataBaseWriter->select($sql);
 
@@ -16,18 +21,41 @@ include 'top.php';
     print '<table id="employees">';
 
     print '<tr>';
-    print '<th>ID</th>';
-    print '<th>Name</th>';
-    print '<th>Salary</th>';
-    print '<th>Degree</th>';
+    if (strcmp($direction, "ASC") == 0) {
+        print '<th><a href="index.php?order=pmkEmployeeId&direction=DESC&page=0">ID</a></th>';
+    } else {
+        print '<th><a href="index.php?order=pmkEmployeeId&direction=ASC&page=0">ID</a></th>';
+    }
+
+
+    if (strcmp($direction, "ASC") == 0) {
+        print '<th><a href="index.php?order=fldName&direction=DESC&page=0">Name</a></th>';
+    } else {
+        print '<th><a href="index.php?order=fldName&direction=ASC&page=0">Name</a></th>';
+    }
+
+
+    if (strcmp($direction, "ASC") == 0) {
+        print '<th><a href="index.php?order=fldSalary&direction=DESC&page=0">Salary</a></th>';
+    } else {
+        print '<th><a href="index.php?order=fldSalary&direction=ASC&page=0">Salary</a></th>';
+    }
+
+    if (strcmp($direction, "ASC") == 0) {
+        print '<th><a href="index.php?order=fldHighestDegree&direction=DESC&page=0">Degree</a></th>';
+    } else {
+        print '<th><a href="index.php?order=fldHighestDegree&direction=ASC&page=0">Degree</a></th>';
+    }
+
+
     print '</tr>';
 
-    foreach ($records as $record) {
+    for (int i = $page; i < $page+10; $page++) {
         print '<tr>' . PHP_EOL;
-        print '<td>' . $record['pmkEmployeeId'] . '</td>' . PHP_EOL;
-        print '<td>' . $record['fldName'] . '</td>' . PHP_EOL;
-        print '<td>' . $record['fldSalary'] . '</td>' . PHP_EOL;
-        print '<td>' . $record['fldHighestDegree'] . '</td>' . PHP_EOL;
+        print '<td>' . $records[i]['pmkEmployeeId'] . '</td>' . PHP_EOL;
+        print '<td>' . $records[i]['fldName'] . '</td>' . PHP_EOL;
+        print '<td>' . $records[i]['fldSalary'] . '</td>' . PHP_EOL;
+        print '<td>' . $records[i]['fldHighestDegree'] . '</td>' . PHP_EOL;
         print '</tr>' . PHP_EOL;
     }
 
